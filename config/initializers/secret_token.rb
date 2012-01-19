dbd22c8830f4637f7606215ddcafe9e66b7ef2bb
Alberto Contreras <alberto@laciudadaccesible.com>
2012-01-19 17:32:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaciudadaccesibleDemo::Application.config.secret_token = '08df4b421e6334464b0a405737363e6434798ccbe8c17ea258866be2b89b78ddb89052dc22bcf712e9643caba488cd327f2aa24093d1af5fb6053b929cf972d5'
